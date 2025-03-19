76cbbffb3188352804bc92b1ad4a573b478e038e
Anas Halbawi <anashalb@gmail.com>
2025-03-19 16:13:42 +0400

    chore: add unit tests

---
+++ recipeService/src/test/java/com/thepantry/recipeservice/persistence/UnitConfigurationTests.java
@@ -0,0 +1,51 @@
+package com.thepantry.recipeservice.persistence;
+
+import com.thepantry.recipeservice.infrastructure.persistence.UnitConfiguration;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitConfigurationTests {
+
+    private final UnitConfiguration unitConfiguration = new UnitConfiguration();
+
+    @Test
+    public void testGetUnitInfo_ShouldReturnUnitInfo_WhenImperialUnitExists() {
+
+        String imperialUnit = unitConfiguration.getAllImperialUnits().stream().findFirst().get();
+        UnitConfiguration.UnitInfo unitInfo = unitConfiguration.getUnitInfo(imperialUnit);
+        assertEquals(imperialUnit, unitInfo.shortName());
+    }
+
+    @Test
+    public void testGetUnitInfo_ShouldReturnUnitInfo_WhenMetricUnitExists() {
+
+        String metricUnit = unitConfiguration.getAllMetricUnits().stream().findFirst().get();
+        UnitConfiguration.UnitInfo unitInfo = unitConfiguration.getUnitInfo(metricUnit);
+        assertEquals(metricUnit, unitInfo.shortName());
+    }
+
+    @Test
+    public void testGetUnitInfo_ShouldReturnNull_WhenUnitDoesNotExist() {
+        assertNull(unitConfiguration.getUnitInfo("invalidUnit"));
+    }
+
+    @Test
+    public void testIsValidUnit_ShouldReturnTrue_WhenImperialUnitExists() {
+
+        String imperialUnit = unitConfiguration.getAllImperialUnits().stream().findFirst().get();
+        assertTrue(unitConfiguration.isValidUnit(imperialUnit));
+    }
+
+    @Test
+    public void testIsValidUnit_ShouldReturnTrue_WhenMetricUnitExists() {
+
+        String metricUnit = unitConfiguration.getAllMetricUnits().stream().findFirst().get();
+        assertTrue(unitConfiguration.isValidUnit(metricUnit));
+    }
+
+    @Test
+    public void testIsValidUnit_ShouldReturnFalse_WhenUnitDoesNotExist() {
+        assertFalse(unitConfiguration.isValidUnit("invalidUnit"));
+    }
+}
